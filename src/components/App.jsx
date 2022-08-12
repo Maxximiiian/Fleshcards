2d7a563faed1d70269b073f1221b202b861894e2
Maxximiiian <SantoCastor@yandex.ru>
2022-08-12 12:59:31 +0300

    auth 0.5

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Navbar from './navbar/Navbar';
+import MainPage from './mainpage/MainPage';
 
 export default function App() {
   return (
     <div className="container">
       <Navbar />
       <Routes>
-        <Route path="/" />
+        <Route path="/" element={<MainPage />} />
         <Route path="/choise" />
         <Route path="/choise/:id" />
       </Routes>
-      <div>che nibyd</div>
     </div>
   );
 }
--- src/components/mainpage/MainPage.jsx
+++ src/components/mainpage/MainPage.jsx
@@ -1,20 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function MainPage() {
-  const authHandler = async (event) => {
+  const [input, setInput] = useState('');
+
+  const submitHandler = async (event) => {
     event.preventDefault();
+    console.log('ugbuygbuybbh');
   };
+
+  const changeHandler = (e) => {
+    setInput((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+    console.log(input);
+  };
+
   return (
     <div>
       <div className="container">
         <div className="mx-auto mt-5" style={{ width: '400px' }}>
           {/* <div style={{ height: '250px' }} /> */}
-          <form className="container bg-light rounded-3 py-3 item" align="center">
+          <form
+            className="container bg-light rounded-3 py-3 item"
+            align="center"
+            onSubmit={submitHandler}
+          >
             <div className="mb-3">
-              <h2 className="text-dark">dfgdgfdfg</h2>
-              <h2 className="text-dark">dfgdgdf</h2>
+              <h2 className="text-dark">Введи запретное слово</h2>
+
+              <input
+                type="text"
+                name="name"
+                className="form-control"
+                id="Input"
+                value={input.name}
+                onChange={changeHandler}
+                placeholder="BAD WORD"
+              />
             </div>
-            <button type="submit" className="btn btn-success mx-3" onClick={authHandler}>Done!</button>
+            <button type="submit" className="btn btn-success mx-3">Done!</button>
           </form>
         </div>
       </div>
