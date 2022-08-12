9e382b6599f3f278df6d02c4fb02232975566f63
Nikita <djneel.nz@gmail.com>
2022-08-12 15:27:35 +0300

    Sound Join in Project!

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,23 @@
+/* eslint-disable react/button-has-type */
+/* eslint-disable react/jsx-no-bind */
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import play from 'audio-play';
+import load from 'audio-loader';
 
 export default function Navbar() {
+  const playHandler = async function () {
+    const sound = await load('/sound/matrix.mp3');
+    play(sound);
+  };
+
   return (
     <nav className="navbar navbar-light bg-light rounded-3 item">
       <h2 className="text-dark my-auto mx-3">Welcome to MeMeLaNd!</h2>
-      <NavLink to="/" className="btn btn-secondary my-auto align-self-right mx-3">Главная</NavLink>
-      <NavLink to="/" className="btn btn-secondary my-auto align-self-right mx-3">Че нибудь</NavLink>
-      <NavLink to="/" className="btn btn-secondary my-auto align-self-right mx-3">Че нибудь</NavLink>
+      <NavLink onClick={playHandler} to="/" className="btn btn-secondary my-auto align-self-right mx-3">Главная</NavLink>
+      <NavLink onClick={playHandler} to="/" className="btn btn-secondary my-auto align-self-right mx-3">Че нибудь</NavLink>
+      <NavLink onClick={playHandler} to="/" className="btn btn-secondary my-auto align-self-right mx-3">Че нибудь</NavLink>
+      <button onClick={playHandler} className="btn btn-secondary my-auto align-self-right mx-3">PRESS</button>
     </nav>
   );
 }
